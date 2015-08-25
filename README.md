590d3f2b8da5e54fcc52c541df7bfc9348e4609e
Micah Geisel <micah@botandrose.com>
2015-08-25 15:05:01 -0700

    refactor ember helper registration to ember initializer and use new Ember.Helper.helper API.

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ This will output `<img src="/assets/logo.png" />` in development, and `<img src=
 
 ## Requirements
 
-This gem supports Rails 4.x and up. A PR to support Rails 3.x would be greatly appreciated.
+This gem supports Rails 4.x and Ember 1.13.x and up.
 
 ## Contributing
 
--- app/assets/javascripts/ember-rails-assets.js
+++ app/assets/javascripts/ember-rails-assets.js
@@ -1,4 +1,11 @@
-Ember.Handlebars.registerBoundHelper("asset-path", function(name) {
-  var path = window.ASSETS.path(name);
-  return new Ember.Handlebars.SafeString(path);
+Ember.Application.initializer({
+  name: "ember-rails-assets-helpers",
+
+  initialize: function(container, application) {
+    application.register("helper:asset-path", Ember.Helper.helper(function(params, hash) {
+      var name = params[0];
+      var path = window.ASSETS.path(name);
+      return new Ember.Handlebars.SafeString(path);
+    }));
+  }
 });
