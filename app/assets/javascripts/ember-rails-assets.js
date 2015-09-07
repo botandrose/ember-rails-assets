9b7bfabf6adf46aaccfc43011f583b1b758cc2d9
Micah Geisel <micah@botandrose.com>
2015-09-07 12:33:04 -0700

    support whole range of Ember releases without deprecation warnings. Closes #6.

--- app/assets/javascripts/ember-rails-assets.js
+++ app/assets/javascripts/ember-rails-assets.js
@@ -2,10 +2,17 @@ Ember.Application.initializer({
   name: "ember-rails-assets-helpers",
 
   initialize: function(container, application) {
-    application.register("helper:asset-path", Ember.Helper.helper(function(params, hash) {
-      var name = params[0];
-      var path = window.ASSETS.path(name);
-      return new Ember.Handlebars.SafeString(path);
-    }));
+    if(typeof Ember.Helper !== "undefined") { // Ember >= 1.13
+      application.register("helper:asset-path", Ember.Helper.helper(function(params, hash) {
+        var name = params[0];
+        var path = window.ASSETS.path(name);
+        return new Ember.Handlebars.SafeString(path);
+      }));
+    } else { // Ember < 1.13
+      Ember.Handlebars.registerBoundHelper("asset-path", function(name) {
+        var path = window.ASSETS.path(name);
+        return new Ember.Handlebars.SafeString(path);
+      });
+    }
   }
 });
